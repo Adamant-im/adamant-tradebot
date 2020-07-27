61924a48a7cd89df74a70a276aefcd188e01a8e7
Aleksei Lebedev <msg@adamant.im>
2020-07-27 16:48:16 +0300

    Default tradeParams for Atomars

--- trade/tradeParams_atomars.js
+++ trade/tradeParams_atomars.js
@@ -1,10 +1,10 @@
 module.exports = {
    "mm_buyPercent": 0.77,
-   "mm_minInterval": 1000,
-   "mm_maxInterval": 20000,
+   "mm_minInterval": 3000,
+   "mm_maxInterval": 350000,
    "mm_isActive": false,
-   "mm_minAmount": 0.0001,
-   "mm_maxAmount": 0.0002,
+   "mm_minAmount": 14,
+   "mm_maxAmount": 1600,
    "mm_isOrderBookActive": true,
    "mm_orderBookHeight": 25,
    "mm_orderBookOrdersCount": 20
