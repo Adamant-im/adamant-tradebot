8882253faff4587198c1cf6a9b185b79860c1be0
Aleksei Lebedev <msg@adamant.im>
2020-07-27 19:05:40 +0300

    Bump version

--- trade/tradeParams_resfinex.js
+++ trade/tradeParams_resfinex.js
@@ -2,7 +2,7 @@ module.exports = {
    "mm_buyPercent": 0.77,
    "mm_minInterval": 3000,
    "mm_maxInterval": 350000,
-   "mm_isActive": true,
+   "mm_isActive": false,
    "mm_minAmount": 14,
    "mm_maxAmount": 1600,
    "mm_isOrderBookActive": true,
