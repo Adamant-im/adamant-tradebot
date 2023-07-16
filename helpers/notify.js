e9f0bb750cad7fb9a29204530ca42f8eff8eb302
confleux <confleux@gmail.com>
2023-07-16 14:14:15 +0300

    chore: use arrow function as argument

--- helpers/notify.js
+++ helpers/notify.js
@@ -51,7 +51,7 @@ module.exports = (messageText, type, silent_mode = false, isPriority = false) =>
         slackKeys.forEach((slackApp) => {
           if (typeof slackApp === 'string' && slackApp.length > 34) {
             axios.post(slackApp, params)
-                .catch(function(error) {
+                .catch((error) => {
                   log.log(`Request to Slack with message ${message} failed. ${error}.`);
                 });
           }
