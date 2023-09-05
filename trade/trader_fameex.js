da0ddedfffa1ef65a1be032de08880c2153ba029
YuriyVorobyov96 <darthrevan1333@gmail.com>
2023-09-05 21:18:02 +0300

    chore: add fameex trader placeholder

---
+++ trade/trader_fameex.js
@@ -0,0 +1,22 @@
+const FameEXApi = require('./api/fameex_api');
+const utils = require('../helpers/utils');
+const networks = require('../helpers/cryptos/networks');
+
+/**
+ * API endpoints:
+ * https://api.fameex.com
+ */
+const apiServer = 'https://api.fameex.com';
+const exchangeName = 'FameEX';
+
+module.exports = (
+    apiKey,
+    secretKey,
+    pwd,
+    log,
+    publicOnly = false,
+    loadMarket = true,
+) => {
+  const fameEXApiClient = FameEXApi();
+  fameEXApiClient.setConfig(apiServer, apiKey, secretKey, pwd, log, publicOnly);
+};
