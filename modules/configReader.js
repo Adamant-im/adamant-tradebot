2036164837e4bdb1093be3cf33020a99dff14331
adamant-al <adamantim@yandex.ru>
2023-11-19 10:09:59 +0400

    feat: add projectNamePlain to config

--- modules/configReader.js
+++ modules/configReader.js
@@ -221,8 +221,12 @@ try {
   config.version = require('../package.json').version;
 
   const pathParts = __dirname.split(path.sep);
-  config.projectName = pathParts[pathParts.length - 2].replace(' ', '-');
-  config.projectName = config.projectName.replace('adamant-', '').replace('tradebot', 'TradeBot').replace('coinoptimus', 'CoinOptimus');
+  config.projectNamePlain = pathParts[pathParts.length - 2];
+  config.projectName = config.projectNamePlain
+      .replace(' ', '-')
+      .replace('adamant-', '')
+      .replace('tradebot', 'TradeBot')
+      .replace('coinoptimus', 'CoinOptimus');
 
   const { exec } = require('child_process');
   exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
