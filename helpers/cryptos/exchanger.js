307afbca35766252f8be8aea8ef83f3c8929a898
adamant-al <adamantim@yandex.ru>
2024-08-08 14:00:02 +0400

    refactor(exchanger): simplify and debug

--- helpers/cryptos/exchanger.js
+++ helpers/cryptos/exchanger.js
@@ -34,53 +34,72 @@ module.exports = {
   },
 
   /**
-   * Returns rate for from/to
-   * @param {String} from Like 'ADM'
-   * @param {String} to Like 'ETH'
-   * @return {Number} or NaN or undefined
+   * Returns the price for the 'from' currency. E.g., 1 ADM = 0.03 USDT.
+   * @param {string} from Currency to get rate. E.g., 'ADM'.
+   * @param {string} to Notional currency. E.g., 'USDT'.
+   * @return {number} The coin price or undefined
    */
   getRate(from, to) {
     try {
-      if (from && to && from === to) return 1; // 1 USD = 1 USD
+      if (!from || !to) return;
+      if (from === to) return 1; // 1 USDT = 1 USDT
+
       let price = this.currencies[from + '/' + to] || 1 / this.currencies[to + '/' + from];
+
       if (!price) {
         // We don't have direct or reverse rate, calculate it from /USD rates
         const priceFrom = this.currencies[from + '/USD'];
         const priceTo = this.currencies[to + '/USD'];
         price = priceFrom / priceTo;
       }
+
+      if (!price) {
+        log.warn(`Unable to calculate the ${from} price in ${to}. Probably, Infoservice doesn't provide rates for one of these.`);
+        return;
+      }
+
       return price;
     } catch (e) {
-      log.error(`Unable to calculate price of ${from} in ${to} in getPrice() of ${utils.getModuleName(module.id)} module: ` + e);
+      log.error(`Unable to calculate the ${from} price in ${to} in the getPrice() of ${utils.getModuleName(module.id)} module: ${e}`);
     }
   },
 
   /**
    * Returns value of amount 'from' currency in 'to' currency
-   * @param {String} from Like 'ADM'
-   * @param {String} to Like 'ETH'
-   * @param {Number} amount Amount of 'from' currency
-   * @param {Boolean} considerExchangerFee If false, do direct market calculation.
-      * If true, deduct the exchanger's and blockchain fees
-   * @return {Number|Number} or { NaN, NaN }
+   * @param {string} from Currency to convert from. E.g., 'ADM'.
+   * @param {string} to Currency to convert to. E.g., 'ETH'.
+   * @param {number} [amount=1] Amount of 'from' currency
+   * @param {boolean} [considerExchangerFee=false] Whether to deduct fees (for the ADAMANT Exchanger bot)
+   * @param {number} [specificRate] Convert using specific rate
+   * @param {boolean} [validateSpecificRate=true] Whether to validate that specificRate differs from the global (Infoservice) no more, than allowed %.
+   * @return {number|number} or { NaN, NaN }
    */
-  convertCryptos(from, to, amount = 1, considerExchangerFee = false, specificRate) {
+  convertCryptos(from, to, amount = 1, considerExchangerFee = false, specificRate, validateSpecificRate = true) {
+    const ALLOWED_GLOBAL_RATE_DIFFERENCE_PERCENT = 20;
+
     try {
-      const ALLOWED_GLOBAL_RATE_DIFFERENCE_PERCENT = 20;
       from = from.toUpperCase();
       to = to.toUpperCase();
+
       let rate = this.getRate(from, to);
+
       if (utils.isPositiveNumber(specificRate)) {
-        const rateDifferencePercent = utils.numbersDifferencePercent(rate, specificRate);
-        if (rateDifferencePercent > ALLOWED_GLOBAL_RATE_DIFFERENCE_PERCENT) {
-          log.warn(`Specific and calculated ${from}/${to} rates differs too much: ${specificRate.toFixed(8)} and ${rate.toFixed(8)} (${rateDifferencePercent.toFixed(2)}%). Refusing to convert.`);
-          return {
-            outAmount: NaN,
-            exchangePrice: NaN,
-          };
+        if (validateSpecificRate) {
+          const rateDifferencePercent = utils.numbersDifferencePercent(rate, specificRate);
+
+          if (rateDifferencePercent > ALLOWED_GLOBAL_RATE_DIFFERENCE_PERCENT) {
+            log.warn(`Provided specific rate and the global ${from}/${to} rate differ too much: ${specificRate} and ${rate} (${rateDifferencePercent.toFixed(2)}%). Refusing to convert.`);
+
+            return {
+              outAmount: NaN,
+              exchangePrice: NaN,
+            };
+          }
         }
+
         rate = specificRate;
       }
+
       let networkFee = 0;
       if (considerExchangerFee) {
         rate *= 1 - config['exchange_fee_' + from] / 100;
@@ -89,13 +108,16 @@ module.exports = {
           networkFee = this.convertCryptos('ETH', to, networkFee).outAmount;
         }
       }
-      const value = rate * +amount - networkFee;
+
+      const value = rate * amount - networkFee;
+
       return {
-        outAmount: +value.toFixed(constants.PRECISION_DECIMALS),
-        exchangePrice: +rate.toFixed(constants.PRECISION_DECIMALS),
+        outAmount: value,
+        exchangePrice: rate,
       };
     } catch (e) {
-      log.error(`Unable to calculate ${amount} ${from} in ${to} in convertCryptos() of ${utils.getModuleName(module.id)} module: ` + e);
+      log.error(`Unable to calculate ${amount} ${from} in ${to} in convertCryptos() of ${utils.getModuleName(module.id)} module: ${e}`);
+
       return {
         outAmount: NaN,
         exchangePrice: NaN,
